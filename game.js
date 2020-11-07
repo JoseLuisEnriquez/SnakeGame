6829ffb4f1e37cbd98d267db1b60db6ba1d870ad
Jose Luis Enriquez <josl.enriquez89@gmail.com>
2020-11-07 19:49:14 -0300

    Radium PS06 - Step 01.05 Interaction of several elements

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
-var canvas = null,
+var key_left = 37, key_up = 38, key_right = 39, key_down = 40, key_enter = 13,
+    canvas = null,
     ctx = null,
     player = null,
     lastPress = null,
-    key_left = 37, key_up = 38, key_right = 39, key_down = 40, key_enter = 13,
     dir = 0,
     pause = true,
+    gameover = true,
     score = 0,
-    food = null;
+    food = null,
+    wall = new Array();
 
 window.requestAnimationFrame = (function(){
     return window.requestAnimationFrame ||
@@ -49,13 +51,30 @@ function random(max){
     return Math.floor(Math.random() * max);
 }
 
+function reset(){
+    score = 0;
+    dir = 1;
+    player.x = 40;
+    player.y = 40;
+    food.x = random(canvas.width / 10 - 1) * 10;
+    food.y = random(canvas.height / 10 - 1) * 10;
+    gameover = false;
+}
+
 function paint(ctx){
+    var i = 0,
+        l = 0;
     //Clean canvas
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     //Draw player
     ctx.fillStyle = '#0f0';
     player.fill(ctx);
+    //Draw walls
+    ctx.fillStyle = '#999';
+    for (i = 0, l = wall.length; i < l; i += 1){
+        wall[i].fill(ctx);
+    }
     //Draw food
     ctx.fillStyle = '#f00';
     food.fill(ctx);
@@ -67,13 +86,23 @@ function paint(ctx){
     //Draw pause
     if(pause){
         ctx.textAling = 'center';
+        if(gameover){
+            ctx.fillStyle = '#f00';
+            ctx.fillText('GAME OVER', 300, 125);
+        } else {
         ctx.fillText('PAUSE', 300, 125);
+        }
         ctx.textAling = 'left';
     }
 }
 
 function act(){
+    var i, l;
     if(!pause){
+        //Gameover Reset
+        if(gameover){
+            reset();
+        }
         //Change direction
         if(lastPress == key_up){
             dir = 0;
@@ -119,6 +148,17 @@ function act(){
             food.x = random(canvas.width / 10 - 1) * 10;
             food.y = random(canvas.height / 10 - 1) * 10;
         }
+        //Wall intersects
+        for(i = 0, l = wall.length; i < l; i += 1){
+            if(food.intersects(wall[i])){
+                food.x = random(canvas.width / 10 - 1) * 10;
+                food.y = random(canvas.height / 10 - 1) * 10;
+            }
+            if(player.intersects(wall[i])){
+                gameover = true;
+                pause = true;
+            }
+        }
     }
     //Paused - Unpaused
     if(lastPress == key_enter){
@@ -144,6 +184,11 @@ function init(){
     //Create player and food
     player = new Rectangle(40, 40, 10, 10);
     food = new Rectangle(80, 80, 10, 10);
+    //Create Walls
+    wall.push(new Rectangle(100, 50, 10, 10));
+    wall.push(new Rectangle(100, 100, 10, 10));
+    wall.push(new Rectangle(200, 50, 10, 10));
+    wall.push(new Rectangle(200, 100, 10, 10));
     //Start game
     run();
     repaint();
