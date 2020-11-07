1bdc35df5343b8b4f0a90ba0f806942d905aacdb
Jose Luis Enriquez <josl.enriquez89@gmail.com>
2020-11-07 17:27:33 -0300

    Radium PS06 - Step 01.04 Interactios between elements

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 var canvas = null,
     ctx = null,
-    x = 50,
-    y = 50,
+    player = null,
     lastPress = null,
     key_left = 37, key_up = 38, key_right = 39, key_down = 40, key_enter = 13,
     dir = 0,
-    pause = true;
+    pause = true,
+    score = 0,
+    food = null;
 
 window.requestAnimationFrame = (function(){
     return window.requestAnimationFrame ||
@@ -20,15 +21,49 @@ document.addEventListener('keydown', function(evt){
     lastPress = evt.which;
 }, false);
 
+function Rectangle(x, y, width, height){
+    this.x = (x == null) ? 0 : x;
+    this.y = (y == null) ? 0 : y;
+    this.width = (width == null) ? 0 : width;
+    this.height = (height == null) ? this.width : height;
+    this.intersects = function (rect){
+        if(rect == null){
+            window.console.warn('Missing parameters on function intersects');
+        } else {
+            return(this.x < rect.x + rect.width &&
+                this.x + this.width > rect.x &&
+                this.y < rect.y + rect.height &&
+                this.y + this.height > rect.y);
+        }
+    };
+    this.fill = function(ctx){
+        if(ctx == null){
+            window.console.warn('Missing parameter on function fill');
+        } else {
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    };
+}
+
+function random(max){
+    return Math.floor(Math.random() * max);
+}
+
 function paint(ctx){
     //Clean canvas
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //Draw square
+    //Draw player
     ctx.fillStyle = '#0f0';
-    ctx.fillRect(x, y, 10, 10);
+    player.fill(ctx);
+    //Draw food
+    ctx.fillStyle = '#f00';
+    food.fill(ctx);
     //Debug last key pressed
-    ctx.fillText('Last press: ' + lastPress, 0, 20);
+    ctx.fillStyle = '#fff';
+    //ctx.fillText('Last press: ' + lastPress, 0, 20);
+    //Draw score
+    ctx.fillText('Score: ' + score, 0, 10);
     //Draw pause
     if(pause){
         ctx.textAling = 'center';
@@ -54,29 +89,35 @@ function act(){
         }
         //Move Rect
         if(dir == 0){
-            y -= 10;
+            player.y -= 10;
         }
         if(dir == 1){
-            x += 10;
+            player.x += 10;
         }
         if(dir == 2){
-            y += 10;
+            player.y += 10;
         }
         if(dir == 3){
-            x -= 10;
+            player.x -= 10;
         }
         //Out Screen
-        if(x > canvas.width){
-            x = 0;
+        if(player.x > canvas.width){
+            player.x = 0;
         }
-        if(y > canvas.height){
-            y = 0;
+        if(player.y > canvas.height){
+            player.y = 0;
         }
-        if(x < 0){
-            x = canvas.width;
+        if(player.x < 0){
+            player.x = canvas.width;
         }
-        if(y < 0){
-            y = canvas.height;
+        if(player.y < 0){
+            player.y = canvas.height;
+        }
+        //Food intersects
+        if(player.intersects(food)){
+            score += 1;
+            food.x = random(canvas.width / 10 - 1) * 10;
+            food.y = random(canvas.height / 10 - 1) * 10;
         }
     }
     //Paused - Unpaused
@@ -100,7 +141,9 @@ function init(){
     //Get canvas and context
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-
+    //Create player and food
+    player = new Rectangle(40, 40, 10, 10);
+    food = new Rectangle(80, 80, 10, 10);
     //Start game
     run();
     repaint();
